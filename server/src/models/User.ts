75cee6ed4c0d8e53072f6610afa969831cc25532
vlad_vitkovskyi <vvitkovskyi4@gmail.com>
2024-08-03 18:38:11 +0300

    feature/add-login-logic

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,23 +1,41 @@
-const mongoose = require("mongoose");
+import { Schema, model } from "mongoose";
 
-const Schema = mongoose.Schema;
+export type UserSchemaType = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  username: string;
+  password: string;
+  refresh_token: string;
+};
 
-const UserSchema = new Schema({
-  first_name: {
-    type: String,
-    required: true,
+const UserSchema = new Schema<UserSchemaType>(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refresh_token: String,
   },
-  last_name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  refresh_token: String,
-});
+  {
+    strict: true,
+  }
+);
+
+export const User = model("Users", UserSchema);
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,9 +1,12 @@
 import express from "express";
 
 import { signup } from "../controllers/auth/signup";
+import { login } from "../controllers/auth/login";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+
+router.post("/login", login);
 
 export default router;
